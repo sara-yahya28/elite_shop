f0a376bf86a048c21f7bbf81c465eb3b002fee0e
Sara <sara.yahyamqbl@gmail.com>
2026-08-28 16:34:18 +0300

    Add login and sign-up pages with input validation and terms agreement

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:elite_shop/pages/auth/signin_page.dart';
+import 'package:elite_shop/pages/auth/welcome_page.dart';
 import 'package:flutter/material.dart';
 import 'package:device_preview/device_preview.dart';
 import 'package:flutter/foundation.dart';
@@ -8,10 +10,7 @@ import 'package:elite_shop/pages/cart/cart_page.dart';
 
 void main() {
   runApp(
-    DevicePreview(
-      enabled: !kReleaseMode,
-      builder: (context) => const MyApp(),
-    ),
+    DevicePreview(enabled: !kReleaseMode, builder: (context) => const MyApp()),
   );
 }
 
@@ -21,19 +20,20 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      useInheritedMediaQuery: true,   
+      useInheritedMediaQuery: true,
       locale: DevicePreview.locale(context),
       builder: DevicePreview.appBuilder,
-      
-      // 🔽 باقي خصائص التطبيق 🔽
+
       title: 'Elite Shop',
       theme: appTheme(),
       debugShowCheckedModeBanner: false,
       initialRoute: '/',
       routes: {
-        '/': (context) => const LoginPage(),   
+        '/': (context) => const WelcomePage(),
         '/home': (context) => const HomePage(),
         '/cart': (context) => const CartPage(),
+        '/signin': (context) => const SigninPage(),
+        '/login': (context) => const LoginPage(),
       },
     );
   }
---
+++ lib/pages/auth/welcome_page.dart
@@ -0,0 +1,58 @@
+import 'package:elite_shop/pages/auth/login_page.dart';
+import 'package:elite_shop/pages/auth/signin_page.dart';
+import 'package:elite_shop/utils/theme.dart';
+import 'package:flutter/material.dart';
+
+class WelcomePage extends StatefulWidget {
+  const new({super.key});
+
+  @override
+  State<WelcomePage> createState() => _WelcomePageState();
+}
+
+class _WelcomePageState extends State<WelcomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: backgroundColor,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(
+              'assets/elite-store-logo.png',
+              width: 150,
+              height: 150,
+              fit: BoxFit.fill,
+            ),
+            SizedBox(height: 5),
+            Text(
+              ' وجهتك الأولى للتسوق الذكي',
+              style: TextStyle(color: textColor),
+            ),
+            SizedBox(height: 40),
+            ElevatedButton(onPressed: () {
+              Navigator.push(context,
+               MaterialPageRoute(builder: (context)=>LoginPage()));
+            },
+             child: Text('تسجيل الدخول')),
+            SizedBox(height: 10),
+            ElevatedButton(
+              style: OutlinedButton.styleFrom(
+                side: BorderSide(color: primaryColor),
+                backgroundColor: Colors.white),
+              onPressed: () {
+                Navigator.push(context,
+                   MaterialPageRoute(builder: (context)=>SigninPage()));
+              },
+              child: Text('انشاء حساب جديد ',
+              style: TextStyle(
+                color: textColor),
+                ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
